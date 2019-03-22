a7d414fbd94753e67a26969389c5fd52cb7adfe8
Todd Bowden <toddhokie@gmail.com>
2019-03-22 14:53:16 -0400

    Empty project

---
+++ EosioSwiftVault/EosioSwiftVault.h
@@ -0,0 +1,19 @@
+//
+//  EosioSwiftVault.h
+//  EosioSwiftVault
+//
+//  Created by Todd Bowden on 3/22/19.
+//  Copyright © 2018-2019 block.one.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EosioSwiftVault.
+FOUNDATION_EXPORT double EosioSwiftVaultVersionNumber;
+
+//! Project version string for EosioSwiftVault.
+FOUNDATION_EXPORT const unsigned char EosioSwiftVaultVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EosioSwiftVault/PublicHeader.h>
+
+
