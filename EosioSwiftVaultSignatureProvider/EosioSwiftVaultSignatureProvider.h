f9426ac61c36e022bd05f815e4a9992ba694bc62
Todd Bowden <toddhokie@gmail.com>
2019-04-09 16:20:33 -0400

    Initial project

---
+++ EosioSwiftVaultSignatureProvider/EosioSwiftVaultSignatureProvider.h
@@ -0,0 +1,19 @@
+//
+//  EosioSwiftVaultSignatureProvider.h
+//  EosioSwiftVaultSignatureProvider
+//
+//  Created by Todd Bowden on 4/9/19.
+//  Copyright (c) 2018-2019 block.one
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EosioSwiftVaultSignatureProvider.
+FOUNDATION_EXPORT double EosioSwiftVaultSignatureProviderVersionNumber;
+
+//! Project version string for EosioSwiftVaultSignatureProvider.
+FOUNDATION_EXPORT const unsigned char EosioSwiftVaultSignatureProviderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EosioSwiftVaultSignatureProvider/PublicHeader.h>
+
+
